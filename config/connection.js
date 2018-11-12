48a32c52567f343e158f79be21f029e3995c98b1
naz585 <naz585@aol.com>
2018-11-11 19:44:31 -0500

    server fix

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+connection = mysql.createConnection({
   host: "localhost",
   port: 3307,
   user: "root",
@@ -9,12 +13,5 @@ var connection = mysql.createConnection({
 });
 
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 module.exports = connection;
